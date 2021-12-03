29d560512f4921751b0c11207bb606e0339c83af
jeremie.db <jeremie.db@evovest.com>
2021-12-03 10:33:54 -0500

    up

--- experiments/launcher.jl
+++ experiments/launcher.jl
@@ -11,7 +11,6 @@ weave("experiments/weave-demo-iris.jmd",
     fig_path = "$(@__DIR__)/output/weave/figs", 
     doctype = "md2html")
 
-
 weave("experiments/weave-demo-iris-bs.jmd", 
     out_path = "$(@__DIR__)/output/weave", 
     fig_path = "$(@__DIR__)/output/weave/figs", 
