17348f62c76f8bbaac1bb2e9643f931690581d15
jeremie.db <jeremie.db@evovest.com>
2021-12-03 17:06:29 -0500

    up

--- bin/main.jl
+++ bin/main.jl
@@ -3,10 +3,9 @@ using Weave
 println("pwd():", pwd())
 
 path = tempdir()
-weave("../experiments/weave-demo-1.jmd", 
+weave("../experiments/weave-demo-iris.jmd", 
     out_path = "$path", 
     fig_path = "$(path)/figs", 
     doctype = "md2html")
 
-ENV["RESULTS"] = "results string"
 ENV["RESULTS_FILE"] = "$(path)/weave-demos-iris.html"
