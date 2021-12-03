63daf7ae67e7a8bce756a197e786b3a2f7fafacc
jeremie.db <jeremie.db@evovest.com>
2021-12-02 19:35:56 -0500

    init

---
+++ experiments/launcher.jl
@@ -0,0 +1,14 @@
+using Weave
+using Literate
+
+weave("experiments/weave-demo-1.jmd", 
+    out_path = "$(@__DIR__)/output/weave", 
+    fig_path = "$(@__DIR__)/output/weave/figs", 
+    doctype = "md2html")
+
+weave("experiments/weave-demo-iris.jmd", 
+    out_path = "$(@__DIR__)/output/weave", 
+    fig_path = "$(@__DIR__)/output/weave/figs", 
+    doctype = "md2html")
+
+Literate.markdown("experiments/literate-demo-1.jl")
---
+++ src/JuliaMdDemos.jl
@@ -0,0 +1,5 @@
+module JuliaMdDemos
+
+greet() = print("Hello World!")
+
+end # module
