494f267fc627a085deb82a86c8b898d83649e90b
jeremie.db <jeremie.db@evovest.com>
2021-12-05 11:45:31 -0500

    up

--- bin/main.jl
+++ bin/main.jl
@@ -1,13 +1,13 @@
 using Weave
 using JSON3
 
-println("pwd():", pwd())
-println("@__DIR__:", @__DIR__)
+println("pwd(): ", pwd())
+println("@__DIR__: ", @__DIR__)
 
 cd(@__DIR__)
 
-println("pwd() 2:", pwd())
-println("@__DIR__ 2:", @__DIR__)
+println("pwd() 2: ", pwd())
+println("@__DIR__ 2: ", @__DIR__)
 
 path = tempdir()
 weave("weave-demo-iris.jmd", 
@@ -19,11 +19,11 @@ open("/home/jrun/results.json", "w") do io
     JSON3.pretty(io, JSON3.write(Dict("tag" => "iris report json")))
 end
 
-cd("/home/jrun")
-@info readdir()
+# cd("/home/jrun")
+# @info readdir()
 @info readdir("/home/jrun")
 
 ENV["RESULTS"] = JSON3.write(Dict("tag" => "iris report"))
-# ENV["RESULTS_FILE"] = "/home/jrun/weave-demos-iris.html"
-ENV["RESULTS_FILE"] = "results.json"
+ENV["RESULTS_FILE"] = "/home/jrun/weave-demos-iris.html"
+# ENV["RESULTS_FILE"] = "results.json"
 # ENV["RESULTS_FILE"] = "weave-demo-iris.jmd"
