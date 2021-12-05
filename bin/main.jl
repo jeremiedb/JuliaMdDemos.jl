344d66d26929e6ebb455c0d04d28a3f607085a62
jeremie.db <jeremie.db@evovest.com>
2021-12-05 16:57:13 -0500

    up

--- bin/main.jl
+++ bin/main.jl
@@ -7,29 +7,19 @@ println("@__DIR__: ", @__DIR__)
 
 cd(@__DIR__)
 
-println("pwd() 2: ", pwd())
-println("@__DIR__ 2: ", @__DIR__)
-
 path_results = "/home/jrun/results"
-# path_figs = "/home/jrun/figs"
+path_fig = "/home/jrun/results/fig"
 
 weave("weave-demo-iris.jmd", 
     out_path = path_results, 
-    fig_path = path_results, 
+    fig_path = path_fig, 
     doctype = "md2html")
 
 open("/home/jrun/results.json", "w") do io
     JSON3.pretty(io, JSON3.write(Dict("tag" => "iris report json")))
 end
 
-# cd("/home/jrun")
-# @info readdir()
-@info readdir("/home/jrun/results")
-
-# ENV["RESULTS"] = JSON3.write(Dict("tag" => "iris report"))
-# ENV["RESULTS_FILE"] = "/home/jrun/weave-demos-iris.html"
-# ENV["RESULTS_FILE"] = "results.json"
-# ENV["RESULTS_FILE"] = "weave-demo-iris.jmd"
+@info readdir(path_results)
 
 tarball = Tar.create(path_results)
 ENV["RESULTS_FILE"] = tarball
