2ebe55c79285dffc6835f37f6e871bf6cde567c7
jeremie.db <jeremie.db@evovest.com>
2021-12-05 00:10:20 -0500

    up

--- bin/main.jl
+++ bin/main.jl
@@ -1,7 +1,10 @@
 using Weave
+using JSON3
 
 println("pwd():", pwd())
 println("@__DIR__:", @__DIR__)
+
+cd(@__DIR__)
 
 path = tempdir()
 weave("weave-demo-iris.jmd", 
@@ -9,4 +12,5 @@ weave("weave-demo-iris.jmd",
     fig_path = "$(path)/figs", 
     doctype = "md2html")
 
+ENV["RESULTS"] = JSON3.write(Dict("tag" => "iris report"))
 ENV["RESULTS_FILE"] = "$(path)/weave-demos-iris.html"
