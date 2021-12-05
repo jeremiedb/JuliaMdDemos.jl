3ef410920fcebb6c581cab2f866a82d927172769
jeremie.db <jeremie.db@evovest.com>
2021-12-05 01:52:26 -0500

    test

--- bin/main.jl
+++ bin/main.jl
@@ -11,13 +11,13 @@ println("@__DIR__ 2:", @__DIR__)
 
 path = tempdir()
 weave("weave-demo-iris.jmd", 
-    out_path = "", 
-    fig_path = "/figs", 
+    out_path = "/home/jrun", 
+    fig_path = "/home/jrun/figs", 
     doctype = "md2html")
 
 @info readdir()
 @info readdir(path)
 
 ENV["RESULTS"] = JSON3.write(Dict("tag" => "iris report"))
-ENV["RESULTS_FILE"] = "weave-demos-iris.html"
+ENV["RESULTS_FILE"] = "/home/jrun/weave-demos-iris.html"
 # ENV["RESULTS_FILE"] = "weave-demo-iris.jmd"
